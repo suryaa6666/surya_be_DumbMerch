d4b3086c32cb3b79ddf155d649ae7109f047d701
suryaa6666 <suryaelidanto@gmail.com>
2022-07-10 14:37:23 +0700

    user routes done

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,39 +1,93 @@
+const { user } = require('../../models');
 
 exports.getUsers = async (req, res) => {
-    res.send({
-        message: "get users"
-    })
+    try {
+        const data = await user.findAll();
+
+        res.status(200).send({
+            status: "success",
+            data
+        })
+
+    } catch (error) {
+        res.status(400).send({
+            status: "error",
+            message: error.toString()
+        })
+    }
 }
 
 exports.getUser = async (req, res) => {
+    try {
+        const { id } = req.params;
 
-    const { id } = req.params;
+        const data = await user.findOne({
+            where: { id }
+        })
 
-    res.send({
-        message: `get user ${id}`
-    })
+        res.status(200).send({
+            status: "success",
+            data
+        })
+    } catch (error) {
+        res.status(400).send({
+            status: "error",
+            message: error.toString()
+        })
+    }
 }
 
 exports.addUser = async (req, res) => {
-    res.send({
-        message: "add user"
-    })
+    try {
+        await user.create(req.body);
+
+        res.status(201).send({
+            message: "new user has been successfully added!"
+        })
+    } catch (error) {
+        res.status(400).send({
+            status: "error",
+            message: error.toString()
+        })
+    }
 }
 
 exports.updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
 
-    const { id } = req.params;
+        await user.update(req.body, {
+            where: { id }
+        })
 
-    res.send({
-        message: `update user ${id}`
-    })
+        res.status(200).send({
+            message: `update user id : ${id}, success!`,
+            data: req.body
+        })
+    } catch (error) {
+        res.status(400).send({
+            status: "error",
+            message: error.toString()
+        })
+    }
 }
 
 exports.deleteUser = async (req, res) => {
+    try {
 
-    const { id } = req.params;
+        const { id } = req.params;
 
-    res.send({
-        message: `delete user ${id}`
-    })
+        await user.destroy({
+            where: { id }
+        })
+
+        res.status(200).send({
+            message: `delete user id : ${id}, success!`
+        })
+    } catch (error) {
+        res.status(400).send({
+            status: "error",
+            message: error.toString()
+        })
+    }
 }
