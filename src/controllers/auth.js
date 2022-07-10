6d10ae1f6fab30316c1bf4f580df102bf82de7f9
suryaa6666 <suryaelidanto@gmail.com>
2022-07-10 20:07:19 +0700

    login done

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -44,7 +44,54 @@ exports.register = async (req, res) => {
 }
 
 exports.login = async (req, res) => {
-    res.send({
-        message: "auth login"
-    })
+    try {
+        const schema = Joi.object({
+            email: Joi.string().email().min(5).required(),
+            password: Joi.string().min(4).required()
+        })
+
+        const { error } = schema.validate(req.body)
+
+        if (error) {
+            return res.status(400).send({
+                status: "error",
+                message: error.details[0].message
+            })
+        }
+
+        const userExist = await user.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+
+        if (!userExist) {
+            return res.status(400).send({
+                status: "error",
+                message: "email not registered!"
+            })
+        }
+
+        if (userExist.password !== req.body.password) {
+            return res.status(400).send({
+                status: "error",
+                message: "password is wrong!"
+            })
+        }
+
+        res.status(200).send({
+            status: "success",
+            message: "login success!",
+            data: {
+                name: userExist.name,
+                email: userExist.email
+            }
+        })
+
+    } catch (error) {
+        res.status(400).send({
+            status: "error",
+            message: error.toString()
+        })
+    }
 }
