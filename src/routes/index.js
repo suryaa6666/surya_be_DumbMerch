4b55458953653e0d42d6d7f49eb630f4408742dd
suryaa6666 <suryaelidanto@gmail.com>
2022-07-10 21:21:59 +0700

    add auth middlewares

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,17 @@ const dotenv = require("dotenv").config();
 
 const router = express.Router();
 
+// controllers
 const { getUsers, getUser, addUser, updateUser, deleteUser } = require('../controllers/user');
 const { register, login } = require('../controllers/auth');
 const { getCategories, addCategory, updateCategory, deleteCategory, getCategory } = require('../controllers/category');
 const { getProducts, getProduct, addProduct, updateProduct, deleteProduct } = require('../controllers/product');
 const { getTransactions, getTransaction, addTransaction } = require('../controllers/transaction');
 
-// user routes
+// middlewares
+const { auth } = require("../middlewares/auth");
+
+// user routes... maybe best for admin
 router.get('/user', getUsers);
 router.get('/user/:id', getUser);
 router.post('/user', addUser); // not necessary but okay for dev, use register instead
@@ -21,22 +25,22 @@ router.post('/login', login);
 router.post('/register', register);
 
 // category routes
-router.get('/category', getCategories);
-router.get('/category/:id', getCategory);
-router.post('/category', addCategory);
-router.patch('/category/:id', updateCategory);
-router.delete('/category/:id', deleteCategory);
+router.get('/category', auth, getCategories);
+router.get('/category/:id', auth, getCategory);
+router.post('/category', auth, addCategory);
+router.patch('/category/:id', auth, updateCategory);
+router.delete('/category/:id', auth, deleteCategory);
 
-// category routes
-router.get('/product', getProducts);
-router.get('/product/:id', getProduct);
-router.post('/product', addProduct);
-router.patch('/product/:id', updateProduct);
-router.delete('/product/:id', deleteProduct);
+// product routes
+router.get('/product', auth, getProducts);
+router.get('/product/:id', auth, getProduct);
+router.post('/product', auth, addProduct);
+router.patch('/product/:id', auth, updateProduct);
+router.delete('/product/:id', auth, deleteProduct);
 
 // transaction routes
-router.get('/transaction', getTransactions);
-router.get('/transaction/:id', getTransaction);
-router.post('/transaction', addTransaction);
+router.get('/transaction', auth, getTransactions);
+router.get('/transaction/:id', auth, getTransaction);
+router.post('/transaction', auth, addTransaction);
 
 module.exports = router;
