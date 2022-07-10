7ebf42b870d59cb8935a635c9d67a38be0cedc47
suryaa6666 <suryaelidanto@gmail.com>
2022-07-10 17:04:35 +0700

    done transaction controller

--- src/controllers/transaction.js
+++ src/controllers/transaction.js
@@ -1,20 +1,113 @@
+const { transaction, product, user } = require("../../models")
+
 exports.getTransactions = async (req, res) => {
-    res.send({
-        message: "get transactions"
-    })
+    try {
+        const data = await transaction.findAll({
+            include: [
+                {
+                    model: product,
+                    as: "product",
+                    attributes: {
+                        exclude: ["createdAt", "updatedAt"]
+                    }
+                },
+                {
+                    model: user,
+                    as: "buyer",
+                    attributes: {
+                        exclude: ["createdAt", "updatedAt"]
+                    }
+                },
+                {
+                    model: user,
+                    as: "seller",
+                    attributes: {
+                        exclude: ["createdAt", "updatedAt"]
+                    }
+                },
+            ],
+            attributes: {
+                exclude: ["idProduct", "idBuyer", "idSeller"]
+            }
+        });
+
+        res.status(200).send({
+            status: "success",
+            data
+        })
+
+    } catch (error) {
+        res.status(400).send({
+            status: "error",
+            message: error.toString()
+        })
+    }
 }
 
 exports.getTransaction = async (req, res) => {
+    try {
+        const { id } = req.params;
 
-    const { id } = req.params;
+        const data = await transaction.findOne({
+            where: { id },
+            include: [
+                {
+                    model: product,
+                    as: "product",
+                    attributes: {
+                        exclude: ["createdAt", "updatedAt"]
+                    }
+                },
+                {
+                    model: user,
+                    as: "buyer",
+                    attributes: {
+                        exclude: ["createdAt", "updatedAt"]
+                    }
+                },
+                {
+                    model: user,
+                    as: "seller",
+                    attributes: {
+                        exclude: ["createdAt", "updatedAt"]
+                    }
+                },
+            ],
+            attributes: {
+                exclude: ["idProduct", "idBuyer", "idSeller"]
+            }
+        })
 
-    res.send({
-        message: `get transaction ${id}`
-    })
+        if (!data) return res.status(400).send({
+            status: "failed",
+            message: "product not found"
+        })
+
+        res.status(200).send({
+            status: "success",
+            data
+        })
+
+    } catch (error) {
+        res.status(400).send({
+            status: "error",
+            message: error.toString()
+        })
+    }
 }
 
 exports.addTransaction = async (req, res) => {
-    res.send({
-        message: "add transaction"
-    })
+    try {
+        await transaction.create(req.body);
+
+        res.status(201).send({
+            status: "success",
+            message: "new transaction has been successfully added!"
+        })
+    } catch (error) {
+        res.status(400).send({
+            status: "error",
+            message: error.toString()
+        })
+    }
 }
